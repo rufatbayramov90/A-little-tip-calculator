bbca0c2ab3c3660730315fdb0f2686113e47c526
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-09-27 12:17:03 +0400

    Current Age

--- python.py
+++ python.py
@@ -3,7 +3,7 @@
 #What was the total bill?
 #How many people to split the bill?
 #what percentage tip would you like to give?
-
+'''
 bill = float(input("Include bill"))
 tip = int(input("How much do you want to bet?"))
 people = int(input("Write the number of people"))
@@ -21,8 +21,13 @@ amount = bill * (tip/100)
 total = (amount + bill)/people
 final = round(total,2)
 print(f"{final}")
+'''
 
-
-
-
-
+#current age
+age = input("What is your current age")
+age_as_int = int(age)
+years_remaining = 90 - age_as_int
+days_remaining = years_remaining * 365
+weeks_remaining = years_remaining * 52
+months_remaining = years_remaining * 12
+print(f"You have {days_remaining} days, {weeks_remaining} weeks, {months_remaining} months ")
