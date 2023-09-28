7ffab6fb83a250fca889271e3359ae8be405791b
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-09-28 08:55:20 +0400

    Rounding to decimal place

--- python.py
+++ python.py
@@ -3,7 +3,7 @@
 #What was the total bill?
 #How many people to split the bill?
 #what percentage tip would you like to give?
-'''
+
 bill = float(input("Include bill"))
 tip = int(input("How much do you want to bet?"))
 people = int(input("Write the number of people"))
@@ -13,16 +13,17 @@ amount = bill * interest
 total = bill + amount
 bill_person = total/people
 final_amount = round(bill_person,2)
+final_amount = "{:.2f}".format(bill_person)
 print(f"{final_amount}")
 
-
+'''
 #----another syntax
 amount = bill * (tip/100)
 total = (amount + bill)/people
 final = round(total,2)
 print(f"{final}")
 '''
-
+'''
 #current age
 age = input("What is your current age")
 age_as_int = int(age)
@@ -31,3 +32,4 @@ days_remaining = years_remaining * 365
 weeks_remaining = years_remaining * 52
 months_remaining = years_remaining * 12
 print(f"You have {days_remaining} days, {weeks_remaining} weeks, {months_remaining} months ")
+'''
